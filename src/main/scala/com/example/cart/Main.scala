b170e95bbec29576dccd2b26f8c0692febcb6e7b
24santoshr <24santoshr@gmail.com>
2025-10-17 16:29:23 +0530

    printing the value in the main function

--- src/main/scala/com/example/cart/Main.scala
+++ src/main/scala/com/example/cart/Main.scala
@@ -7,5 +7,17 @@ import com.example.cart.service.{CartService, ProductService}
 import org.http4s.ember.client.EmberClientBuilder
 
 object Main extends IOApp.Simple {
-  def run: IO[Unit] =  ???
+  def run: IO[Unit] =  EmberClientBuilder.default[IO].build.use { client =>
+    val productService = new ProductService(client)
+    val cartService = new CartService(productService)
+    val emptyCart = Cart(Nil)
+
+    for {
+      cart1 <- cartService.addToCart(emptyCart, "cornflakes", 2)
+      cart2 <- cartService.addToCart(cart1, "weetabix", 1)
+      _     <- IO.println(s"Cart's subtotal is: ${cart2.cartSubtotal}")
+      _     <- IO.println(s"Cart's tax payable is: ${cart2.taxPayable}")
+      _     <- IO.println(s"Cart's total payable value is: ${cart2.totalPayable}")
+    } yield ()
+  }
 }
