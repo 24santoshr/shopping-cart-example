b885fd720613f310854e0a3c29de1e35fc9d0b90
24santoshr <24santoshr@gmail.com>
2026-03-29 21:07:19 +0530

    using lazy vals

--- src/main/scala/com/example/cart/domain/Cart.scala
+++ src/main/scala/com/example/cart/domain/Cart.scala
@@ -3,9 +3,9 @@ package com.example.cart.domain
 import scala.math.BigDecimal.RoundingMode
 
 final case class Cart(items: List[CartItem]) {
-  val cartSubtotal: BigDecimal = (items.map(i => i.product.price * i.quantity).sum).setScale(2,RoundingMode.HALF_UP)
-  val taxPayable: BigDecimal = (cartSubtotal * 0.125).setScale(2,RoundingMode.HALF_UP)
-  val totalPayable: BigDecimal = (cartSubtotal + taxPayable).setScale(2,RoundingMode.HALF_UP)
+  lazy val cartSubtotal: BigDecimal = (items.map(i => i.product.price * i.quantity).sum).setScale(2,RoundingMode.HALF_UP)
+  lazy val taxPayable: BigDecimal = (cartSubtotal * 0.125).setScale(2,RoundingMode.HALF_UP)
+  lazy val totalPayable: BigDecimal = (cartSubtotal + taxPayable).setScale(2,RoundingMode.HALF_UP)
 
 }
 
